f6b57eee7b0f7328e04d0c922758341120b99aa1
tuktduk <patipantuktduk@gmail.com>
2018-03-19 21:53:53 +0530

    aaa

--- src/process/DatabaseHandler.java
+++ src/process/DatabaseHandler.java
@@ -8,51 +8,63 @@ import java.util.Iterator;
 
 import org.bson.Document;
 
-import com.mongodb.MongoClient; 
-import com.mongodb.MongoCredential;  
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
 
-public class DatabaseHandler {   
-   public static void main( String args[] ) {  
-       connect();
-   } 
-   
-   public static void connect() {
-	   
-	   // Creating a Mongo client 
-	   MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
-	   
-	   // Creating Credentials 
-	   MongoCredential credential; 
-	   credential = MongoCredential.createCredential("sampleUser", "myDb","password".toCharArray()); 
-	   System.out.println("Connected to the database successfully");  
-	  
-	  // Accessing the database 
-	  MongoDatabase database = mongo.getDatabase("myDb"); 
-	  System.out.println("Credentials ::"+ credential);
-	  
-	  // Retrieving a collection
-      MongoCollection<Document> collection = database.getCollection("sampleCollection"); 
-      System.out.println("Collection sampleCollection selected successfully");
-      
-      Document document = new Document("title", "MongoDB") 
-      .append("id", 1)
-      .append("description", "database") 
-      .append("likes", 100) 
-      .append("url", "http://www.tutorialspoint.com/mongodb/") 
-      .append("by", "tutorials point");  
-      collection.insertOne(document); 
-      System.out.println("Document inserted successfully");  
-      
-      // Getting the iterable object 
-      FindIterable<Document> iterDoc = collection.find(); 
-      int i = 1; 
+public class DatabaseHandler {
+	public static void main(String args[]) {
+		LoadData();
+	}
 
-      // Getting the iterator 
-      Iterator it = iterDoc.iterator(); 
-    
-      while (it.hasNext()) {  
-         System.out.println(it.next());  
-      i++; 
+	public static void LoadData() {
+
+		// Creating a Mongo client
+		Mongo mongo = new MongoClient("localhost", 27017);
+
+		// Creating Credentials
+		MongoCredential credential;
+		credential = MongoCredential.createCredential("sampleUser", "krupunya", "password".toCharArray());
+		System.out.println("Connected to the database successfully");
+
+		// Accessing the database
+		DB database = mongo.getDB("krupunya");
+		System.out.println("Credentials ::" + credential);
+
+		// Retrieving a collection
+		/*
+		 * DBCollection collection = (DBCollection)
+		 * database.getCollection("sampleCollection");
+		 * System.out.println("Collection sampleCollection selected successfully");
+		 */
+
+		DBCollection collection = database.getCollection("dummyColl");
+
+		// 1. BasicDBObject example
+		System.out.println("BasicDBObject example...");
+		BasicDBObject document = new BasicDBObject();
+		document.put("database", "mkyongDB");
+		document.put("table", "hosting");
+
+		BasicDBObject documentDetail = new BasicDBObject();
+		documentDetail.put("records", 99);
+		documentDetail.put("index", "vps_index1");
+		documentDetail.put("active", "true");
+		document.put("detail", documentDetail);
+
+		collection.insert(document);
+
+		DBCursor cursorDoc = collection.find();
+		while (cursorDoc.hasNext()) {
+			System.out.println(cursorDoc.next());
 		}
+
+		collection.remove(new BasicDBObject());
+
 	}
 }
